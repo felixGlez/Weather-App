450512873151228601f069220717e101611a949d
felixGlez <felixgonzalezgarcia2010@gmail.com>
2023-12-13 19:43:38 +0100

    start

--- src/js/dom.js
+++
@@ -1,6 +0,0 @@
-//SELECTORES
-export const mainElement = document.getElementById('main');
-export const breedsElement = document.getElementById('breeds');
-
-//URLS
-export const breedsURL = 'https://dog.ceo/api/breeds/list/all';
